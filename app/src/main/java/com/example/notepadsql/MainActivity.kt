c8935f6a87a7071a955b75bff9254cd8317a904b
Rustam <gadelshin2011@gmail.com>
2023-02-15 22:47:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/notepadsql/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.notepadsql
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.notepadsql.databinding.ActivityMainBinding
+import com.example.notepadsql.db.MyDbManager
+import kotlinx.coroutines.NonCancellable.children
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val myDbManager = MyDbManager(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onClickListener()
+
+    }
+
+    private fun onClickListener() {
+        binding.button.setOnClickListener {
+            binding.tvTest.text = ""
+            myDbManager.openDb()
+            myDbManager.insertToDb(binding.edTitle.text.toString(), binding.edContent.text.toString())
+            val dataList = myDbManager.readDbData()
+            for (item in dataList) {
+                binding.tvTest.append(item)
+                binding.tvTest.append("\n")
+            }
+        }
+
+    }
+
+    override fun onDestroy() {
+        myDbManager.closeDb()
+        super.onDestroy()
+    }
+
+
+}
